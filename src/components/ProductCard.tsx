917481a681b6bcaf236822fabe07bb71fed7bf89
Placko135 <papplaszlolevente@gmail.com>
2025-01-24 13:08:03 +0100

    Done

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import { Product } from "./types";
+import "../App.css"
+
+const ProductCard = () => {
+    const [searchProduct, setSearchProduct] = useState<string>("");
+    const [product, setProduct] = useState<Product | null>(null);
+    const [error, setError] = useState<string | null>(null);
+    const [productInfo, setProductInfo] = useState<Product[]>([]);
+
+    useEffect(() => {
+        const fetchProductData = async () => {
+          try {
+            const response = await fetch("/products.json");
+            if (!response.ok) {
+              throw new Error("Failed to fetch product data");
+            }
+            const data = await response.json();
+            setProductInfo(data.products);
+          } catch (err) {
+            console.error("Error fetching product data:", err);
+            setError("Failed to load product data.");
+          }
+        };
+        fetchProductData();
+      }, []);
+      
+  
+    const handleSearch = () => {
+
+      const isProductFound = productInfo.find((prod) =>
+        prod.name.toLowerCase().includes(searchProduct.toLowerCase())
+      ); if (isProductFound) {
+        setProduct(isProductFound);
+        setError(null);
+      } else {
+        setProduct(null);
+        setError("No product found with the given name");
+      }
+    };
+  
+    return (
+      <div className="product-card">
+        <div className="search-section">
+            <label>Please enter a product name</label>
+          <input
+            type="text"
+            value={searchProduct}
+            onChange={(e) => setSearchProduct(e.target.value)}
+            placeholder="Enter product name..."
+          />
+          <button onClick={handleSearch}>Search</button>
+        </div>
+        <div className="results-section">
+          {error && <p className="error">{error}</p>}
+          {product && (
+            <div className="product-info">
+              <img className="product-image" src={product.image} alt={product.name} />
+              <div className="product-details">
+                <p>Id: {product.id}</p>
+                <p>Name: {product.name}</p>
+                <p>Price: ${product.price}</p>
+                <p>Category: {product.category}</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  };
+  
+  export default ProductCard;
